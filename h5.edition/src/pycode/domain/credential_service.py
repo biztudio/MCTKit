d3b3c96bc2aa0a90a36d1e0e43fdf443f77fd4f0
Yang SHEN <Yang.Shen@commscope.com>
2017-09-26 17:11:20 +0800

    add credential result enum.

--- h5.edition/src/pycode/domain/credential_service.py
+++ h5.edition/src/pycode/domain/credential_service.py
@@ -1,8 +1,14 @@
 '''
     Credential Domain Service
 '''
+from enum import Enum
 from domain_service import DomainBase
 from credential import CredentialRepository
+
+class Authentication_Result_Enum(Enum):
+    Valid = 1
+    InvalidUsername = 0 
+    InvalidPassword = -1
 
 class CredentialService(DomainBase):
     def __init__(self):
@@ -12,11 +18,11 @@ class CredentialService(DomainBase):
         credential = self.repository.query_credential_by_username(username)        
         if credential:
             if credential.password == password:
-                return 1
+                return Authentication_Result_Enum.Valid
             else:
-                return -1
+                return Authentication_Result_Enum.InvalidPassword
         else:
-            return 0
+            return Authentication_Result_Enum.InvalidUsername
 
 if __name__ == '__main__':
     credential_domain = CredentialService()
