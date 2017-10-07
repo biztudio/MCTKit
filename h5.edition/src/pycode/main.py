732d3df08a575ce54804b277f73d7cac9f7ec2dd
Yang SHEN <biztudio@outlook.com>
2017-10-08 00:28:15 +0800

    NOTE: need to workout how to show list in IE.

--- h5.edition/src/pycode/main.py
+++ h5.edition/src/pycode/main.py
@@ -1,3 +1,6 @@
+'''
+    Entry of this app
+'''
 from flask import render_template
 from flaskapp import app
 from flask_restful import Api
@@ -5,8 +8,9 @@ from config import DebugConfig
 from flask_json import FlaskJSON
 
 import os, sys
-folder = os.path.abspath(os.path.dirname(__file__))
-api_foler = folder + "\\api"
+app_folder = os.path.abspath(os.path.dirname(__file__))
+api_foler = app_folder + "\\api"
+#sys.path.append(app_folder)
 sys.path.append(api_foler)
 print(api_foler)
 from todoitem import TodoItemResource
