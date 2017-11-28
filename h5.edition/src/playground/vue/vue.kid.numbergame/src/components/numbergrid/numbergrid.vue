2058e21a77b7b34c77d7d476a39d36d769cd7e44
Yang SHEN <yang.shen@commscope.com>
2017-11-28 15:56:42 +0800

    update grid style.

--- h5.edition/src/playground/vue/vue.kid.numbergame/src/components/numbergrid/numbergrid.vue
+++ h5.edition/src/playground/vue/vue.kid.numbergame/src/components/numbergrid/numbergrid.vue
@@ -2,14 +2,16 @@
   <div :id='id'>
     <div style="display:none">{{group}}.{{grid_level}}</div>
     <div v-for='(g, gi) in _grids' :style="g.style_config" draggable="true" class="columngroup">
-      <table>
+      <table class="grid_container">
         <thead>
           <tr>
             <th :colspan='g.data[0].length'>第 {{gi+1}} 组</th>
           </tr>
         </thead>
         <tr v-for='(h, i) in g.data'>
-          <td v-for="(r, i) in h" style="width:50px;">{{r}}</td>
+            <td v-for="(r, i) in h" style="border:solid#000 1px;">
+              <div style="width:50px;height:50px;text-align:center;position:relative;top:15px;">{{r}}</div>
+            </td>
         </tr>
       </table>
     </div>
@@ -20,8 +22,16 @@
     cursor: move;
 }
 
+.grid_container{
+    border-collapse:collapse;border:none;
+}
+
+.grid_cell{
+    border:solid#000 1px;
+}
+
 .columngroup {
-    max-width: 300px;
+    max-width: 500px;
 }
 </style>
 
