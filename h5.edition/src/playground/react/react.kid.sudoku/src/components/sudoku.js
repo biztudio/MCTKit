515c5b3d35e59bd85acbd70371912bbf75d8f2d8
Yang SHEN <yang.shen@commscope.com>
2018-02-26 16:49:01 +0800

    add init sudoku component

---
+++ h5.edition/src/playground/react/react.kid.sudoku/src/components/sudoku.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class Sudoku extends Comment{
+    render(){
+        return (
+            <h1>My Sudoku!!!</h1>
+        );
+    }
+}
+
+export default Sudoku;
