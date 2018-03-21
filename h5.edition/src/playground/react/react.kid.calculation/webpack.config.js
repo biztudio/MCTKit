b3bd0f4bf3073af7ecc47f729c7b0f05b72c744e
Yang SHEN <yang.shen@commscope.com>
2018-03-21 17:18:53 +0800

    add webpack.config.js

---
+++ h5.edition/src/playground/react/react.kid.calculation/webpack.config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const paths = require('./paths');
+
+module.exports = {
+    entry: [],
+    output: {
+        
+    }
+};
