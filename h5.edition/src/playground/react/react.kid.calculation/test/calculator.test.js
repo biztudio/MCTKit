33974111eef2ab5547e5ac488b8df9d363b20d09
Yang SHEN <yang.shen@commscope.com>
2018-03-27 17:18:06 +0800

    add work randomlibiz lib (ver: 1.0.0) as a single file.

--- h5.edition/src/playground/react/react.kid.calculation/test/calculator.test.js
+++ h5.edition/src/playground/react/react.kid.calculation/test/calculator.test.js
@@ -1,12 +1,23 @@
 /*jshint esversion: 6 */
-import mathkit from 'randomlibiz';
+import { mathkit, alphabetkit } from 'randomlibiz';
 
 describe('Test Math Kit', () =>{
     test('Test Random Index', () => {
-        //let index = mathkit.get_random_number_index();
-        //expect(index).toBeLessThanOrEqual(8);
-        expect(0).toBe(0)
+        let index = mathkit.get_random_number_index();
+        console.log(index)
+        expect(index).toBeLessThanOrEqual(8);
     });
 
   
+});
+
+describe('Test Alphabet Kit', () => {
+    test('Test Alphabet Arrary Init', () => {
+        let to_upper_case = true;
+        let letter_array = alphabetkit.get_letter_list(to_upper_case);
+        console.log(letter_array)
+        expect(letter_array.length).toBe(26);
+    });
+
+
 });
