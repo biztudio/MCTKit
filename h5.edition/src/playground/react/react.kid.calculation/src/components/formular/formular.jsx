0e7adcc2f773b62a92c125e7ac4e7ea8cfb2b2b1
Yang SHEN <bizbetter@outlook.com>
2018-03-28 11:53:34 +0800

    add more init code

---
+++ h5.edition/src/playground/react/react.kid.calculation/src/components/formular/formular.jsx
@@ -0,0 +1,11 @@
+/*jshint esversion: 6 */
+import React from 'react';
+import './formular.css';
+
+export class Formular extends React.Component{
+    render(){
+        return (
+            <div>1 + 1 = </div>
+        );
+    }
+};
--- h5.edition/src/playground/react/react.kid.calculation/test/calculator.test.js
+++ h5.edition/src/playground/react/react.kid.calculation/test/calculator.test.js
@@ -4,7 +4,6 @@ import { mathkit, alphabetkit } from 'randomlibiz';
 describe('Test Math Kit', () =>{
     test('Test Random Index', () => {
         let index = mathkit.get_random_number_index();
-        console.log(index)
         expect(index).toBeLessThanOrEqual(8);
     });
 
@@ -15,7 +14,7 @@ describe('Test Alphabet Kit', () => {
     test('Test Alphabet Arrary Init', () => {
         let to_upper_case = true;
         let letter_array = alphabetkit.get_letter_list(to_upper_case);
-        console.log(letter_array)
+        console.log(letter_array.join(','));
         expect(letter_array.length).toBe(26);
     });
 
