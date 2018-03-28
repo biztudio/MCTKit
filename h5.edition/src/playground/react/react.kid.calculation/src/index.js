e686a1e2b92bb97a02ac75749ec6cd66aba3c77a
Yang SHEN <bizbetter@outlook.com>
2018-03-28 12:12:33 +0800

    add indexjs

---
+++ h5.edition/src/playground/react/react.kid.calculation/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Formular from './components/formular/formular';
+
+ReactDOM.render(
+    <Formular />, document.getElementById('root')
+);
