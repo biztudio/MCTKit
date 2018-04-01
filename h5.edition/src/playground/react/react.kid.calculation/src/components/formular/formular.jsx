cb7056d53e87374d8bc0971120f42eefdc113d08
Yang SHEN <bizbetter@outlook.com>
2018-04-02 01:07:18 +0800

    add default props values

--- h5.edition/src/playground/react/react.kid.calculation/src/components/formular/formular.jsx
+++ h5.edition/src/playground/react/react.kid.calculation/src/components/formular/formular.jsx
@@ -1,11 +1,23 @@
 /*jshint esversion: 6 */
 import React from 'react';
+import randomlibiz from 'randomlibiz';
 //import './formular.css';
 
 export class FormularTag extends React.Component{
+    constructor(props){
+        super(props);
+        
+    }
+
     render(){
+        console.log(this.props.level + ', ' + this.props.length)
         return (
             <div>1 + 1 = </div>
         );
     }
 };
+
+FormularTag.defaultProps = {
+    length: 10,//how many formular calculation items
+    level: 1//1: less than 10 and only 'add'; 2: less than 10 and 'add' & 'reduce'
+}
